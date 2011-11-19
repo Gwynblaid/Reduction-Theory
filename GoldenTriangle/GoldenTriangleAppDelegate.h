ceeac6515e97120876edb914d5be53ec996eb04f
Skiv <skiv@MacBook-Pro-Skiv.local>
2011-11-19 22:26:23 +0400

    Initial Commit

---
+++ GoldenTriangle/GoldenTriangleAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  GoldenTriangleAppDelegate.h
+//  GoldenTriangle
+//
+//  Created by Skiv on 19.11.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GoldenTriangleAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ GoldenTriangle/GoldenTriangleAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  GoldenTriangleAppDelegate.m
+//  GoldenTriangle
+//
+//  Created by Skiv on 19.11.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "GoldenTriangleAppDelegate.h"
+
+@implementation GoldenTriangleAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end

a006d409afc700f03f2bfce436291106b86e4f60
Skiv <skiv@MacBook-Pro-Skiv.local>
2012-05-22 14:10:13 +0400

    add interface to second part

--- GoldenTriangle/AppController.h
+++ GoldenTriangle/AppController.h
@@ -10,29 +10,48 @@
 
 @class MyOpenGLView;
 @class StepperController;
+@class CoefficientsOfPreparing;
 
 @interface AppController : NSObject<NSComboBoxDelegate>{
     MyOpenGLView* _graphView;
     StepperController* _numGraphsStepperController;
     NSComboBox* _selectedGraph;
+    NSComboBox* _selectedDistribution2;
     NSTextField* _parametr1;
     NSTextField* _parametr2;
+    NSTextField* _parametr2_1;
+    NSTextField* _parametr2_2;
     NSButton* _writeToFileCheckBox;
     NSTextField* _fileNameTextField;
+    CoefficientsOfPreparing* _calculationCoeffModule;
+    
+    NSTextField* _parametrX;
+    NSTextField* _parametrT;
+    
 }
 
 @property (retain) IBOutlet MyOpenGLView* graphView;
 @property (retain) IBOutlet StepperController* numGraphsStepperController;
 @property (retain) IBOutlet NSComboBox* selectedGraph;
+@property (retain) IBOutlet NSComboBox* selectedDistribution2;
 
 @property (retain) IBOutlet NSTextField* parametr1;
 @property (retain) IBOutlet NSTextField* parametr2;
 
+@property (retain) IBOutlet NSTextField* parametr2_1;
+@property (retain) IBOutlet NSTextField* parametr2_2;
+
 @property (retain) IBOutlet NSButton* writeToFileCheckBox;
 @property (retain) IBOutlet NSTextField* fileNameTextField;
+
+@property (retain) IBOutlet NSTextField* parametrX;
+@property (retain) IBOutlet NSTextField* parametrT;
+
+@property (retain) IBOutlet CoefficientsOfPreparing* calculationCoeffModule;
 
 -(IBAction) touchBulidGraphButton:(id)sender;
 -(IBAction) touchCleanButton:(id)sender;
 -(IBAction) clickWriteToFileCheckBox:(id)sender;
+-(IBAction) clickCalculateButton:(id)sender;
 
 @end
--- GoldenTriangle/AppController.m
+++ GoldenTriangle/AppController.m
@@ -9,15 +9,24 @@
 #import "AppController.h"
 #import "MyOpenGLView.h"
 #import "StepperController.h"
+#import "CoefficientsOfPreparing.h"
+#import "ProbalisticLow.h"
+#import "ProbabilityDistribution.h"
 
 @implementation AppController
 @synthesize graphView = _graphView;
 @synthesize numGraphsStepperController = _numGraphsStepperController;
 @synthesize selectedGraph = _selectedGraph;
+@synthesize selectedDistribution2 = _selectedDistribution2;
 @synthesize parametr1 = _parametr1;
 @synthesize parametr2 = _parametr2;
+@synthesize parametr2_1 = _parametr2_1;
+@synthesize parametr2_2 = _parametr2_2;
 @synthesize writeToFileCheckBox = _writeToFileCheckBox;
 @synthesize fileNameTextField = _fileNameTextField;
+@synthesize calculationCoeffModule = _calculationCoeffModule;
+@synthesize parametrT = _parametrT;
+@synthesize parametrX = _parametrX;
 
 - (id)init
 {
@@ -61,6 +70,28 @@
         default:
             break;
     }
+    
+    self.parametr2_1.stringValue = @"";
+    self.parametr2_2.stringValue = @"";
+    switch ([self.selectedDistribution2 indexOfSelectedItem]) {
+        case DRAW_GAMMA_GRAPH:{
+            [self.parametr2_1.cell setPlaceholderString:@"k"];
+            [self.parametr2_2.cell setPlaceholderString:@"θ"];
+            break;
+        }
+        case DRAW_NORMAL_GRAPH:{
+            [self.parametr2_1.cell setPlaceholderString:@"m"];
+            [self.parametr2_2.cell setPlaceholderString:@"σ"];
+            break;
+        }
+        case DRAW_VEIBUL_GRAPH:{
+            [self.parametr2_1.cell setPlaceholderString:@"k"];
+            [self.parametr2_2.cell setPlaceholderString:@"λ"];
+            break;
+        }
+        default:
+            break;
+    }
 }
 
 -(IBAction) touchCleanButton:(id)sender{
@@ -70,6 +101,73 @@
 -(IBAction) clickWriteToFileCheckBox:(id)sender{
     NSLog(@"%d",_writeToFileCheckBox.state);
     [self.fileNameTextField setHidden:!self.writeToFileCheckBox.state];
+}
+
+-(IBAction) clickCalculateButton:(id)sender{
+    //check for empty
+    assert(![self.parametr1.stringValue isEqualToString:@""]);
+    assert(![self.parametr2.stringValue isEqualToString:@""]);
+    assert(![self.parametr2_1.stringValue isEqualToString:@""]);
+    assert(![self.parametr2_2.stringValue isEqualToString:@""]);
+    ProbalisticLow* low1 = [ProbalisticLow new];
+    ProbalisticLow* low2 = [ProbalisticLow new];
+    
+    low1.selectorClass = [ProbabilityDistribution class];
+    low2.selectorClass = [ProbabilityDistribution class];
+    
+    switch ([_selectedGraph indexOfSelectedItem]) {
+        case DRAW_GAMMA_GRAPH:{
+            low1.f_m = @selector(getGammaDensityWithK:eta:andX:);
+            low1.F = @selector(getGammaDestributionFunctionWithK:eta:andX:);
+            break;
+        }
+        case DRAW_NORMAL_GRAPH:{
+            low1.f_m = @selector(getNormalyzeDensityWithM:sigma:andX:);
+            low1.F = @selector(getNormalyzeDestributionFunctionWithM:eta:andX:);
+            break;
+        }
+        case DRAW_VEIBUL_GRAPH:{
+            low1.f_m = @selector(getVeibulDensity:lambda:andX:);
+            low1.F = @selector(getVeibulDestributionFunctionWithK:lambda:andX:);
+            break;
+        }
+        default:
+            break;
+    }
+    
+    low1.parametr1 = [self.parametr1.stringValue doubleValue];
+    low1.parametr2 = [self.parametr2.stringValue doubleValue];
+    
+    switch ([self.selectedDistribution2 indexOfSelectedItem]) {
+        case DRAW_GAMMA_GRAPH:{
+            low2.f_m = @selector(getGammaDensityWithK:eta:andX:);
+            low2.F = @selector(getGammaDestributionFunctionWithK:eta:andX:);
+            break;
+        }
+        case DRAW_NORMAL_GRAPH:{
+            low2.f_m = @selector(getNormalyzeDensityWithM:sigma:andX:);
+            low2.F = @selector(getNormalyzeDestributionFunctionWithM:eta:andX:);
+            break;
+        }
+        case DRAW_VEIBUL_GRAPH:{
+            low2.f_m = @selector(getVeibulDensity:lambda:andX:);
+            low2.F = @selector(getVeibulDestributionFunctionWithK:lambda:andX:);
+            break;
+        }
+        default:
+            break;
+    }
+    
+    low2.parametr1 = [self.parametr2_1.stringValue doubleValue];
+    low2.parametr2 = [self.parametr2_2.stringValue doubleValue];
+    
+    self.calculationCoeffModule.FLow = low1;
+    self.calculationCoeffModule.GLow = low2;
+    assert(![self.parametrX.stringValue isEqualToString:@""]);
+    assert(![self.parametrT.stringValue isEqualToString:@""]);
+    self.calculationCoeffModule.x = [self.parametrX doubleValue];
+    NSLog(@"Result of calculations: %f", [self.calculationCoeffModule calculateNonstationaryAvailabilityFactorWithT:[self.parametrT doubleValue]]);
+    
 }
 
 @end

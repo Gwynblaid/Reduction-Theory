b7e44423117d8299ef8fb911046b073617d40081
Skiv <skiv@MacBook-Pro-Skiv.local>
2012-05-22 09:52:50 +0400

    add non stationary coefficient

--- GoldenTriangle/CoefficientsOfPreparing.h
+++ GoldenTriangle/CoefficientsOfPreparing.h
@@ -8,6 +8,26 @@
 
 #import <Foundation/Foundation.h>
 
-@interface CoefficientsOfPreparing : NSObject
+@class ProbalisticLow;
 
+@interface CoefficientsOfPreparing : NSObject{
+    
+    ProbalisticLow* _FLow;
+    ProbalisticLow* _GLow;
+    double _parametrX;
+    double _parametrT;
+    double tempParametr;
+    
+}
+
+@property (nonatomic, retain) ProbalisticLow* FLow;
+@property (nonatomic, retain) ProbalisticLow* GLow;
+@property (nonatomic, assign) double x;
+@property (nonatomic, assign) double t;
+
+-(double)calculateNonstationaryAvailabilityFactorWithT:(double)t; // нестационарный коэффициент готовности
+-(double)calculateNonstationaryOpperativeAvailabilityFactorWithT:(double)t; //  нестационарный оперативный коэффициент готовности
+
+-(double)calculateStationaryAvailabilityFactorWithT:(double)t; // стационарный коэффициент готовности
+-(double)calculateStationaryOpperativeAvailabilityFactorWithT:(double)t; // стационарный оперативный коэффициент готовности
 @end
--- GoldenTriangle/CoefficientsOfPreparing.m
+++ GoldenTriangle/CoefficientsOfPreparing.m
@@ -7,8 +7,26 @@
 //
 
 #import "CoefficientsOfPreparing.h"
+#import "ProbalisticLow.h"
+#import "MathMehods.h"
+
+
+@interface CoefficientsOfPreparing()
+//additional functions
+-(double)intToNonestationary:(double)x;
+-(double)intToStationary:(double)x;
+-(double)H1:(double)x;
+-(double)complexFunctionFxG:(double)x;
+-(double)intComplexFunction:(double)x;
+-(double)kernelWithX:(double)x andT:(double)t;
+@end
 
 @implementation CoefficientsOfPreparing
+
+@synthesize GLow = _GLow;
+@synthesize FLow = _FLow;
+@synthesize x = _parametrX;
+@synthesize t = _parametrT;
 
 - (id)init
 {
@@ -18,6 +36,73 @@
     }
     
     return self;
+}
+
+// нестационарный коэффициент готовности
+-(double)calculateNonstationaryAvailabilityFactorWithT:(double)t{
+    self.t = t;
+    return 1 - [self.FLow getFWithX:(self.x + t)] + [MathMehods simpsonFromFunction:@selector(intToNonestationary:) selectorTarget:self isStatic:NO withBorder:CGPointMake(0, t) andHalfNumSteps:100000];
+}
+
+//  нестационарный оперативный коэффициент готовности
+-(double)calculateNonstationaryOpperativeAvailabilityFactorWithT:(double)t{
+    double temp_x = self.x;
+    self.x = 0;
+    double res = [self calculateNonstationaryAvailabilityFactorWithT:t];
+    self.x = temp_x;
+    return res;
+}
+
+// стационарный коэффициент готовности
+-(double)calculateStationaryAvailabilityFactorWithT:(double)t{
+    return 0; 
+}
+
+//  стационарный оперативный коэффициент готовности
+-(double)calculateStationaryOpperativeAvailabilityFactorWithT:(double)t{
+    double temp_x = self.x;
+    self.x = 0;
+    double res = [self calculateStationaryAvailabilityFactorWithT:t];
+    self.x = temp_x;
+    return res;
+}
+
+//private methods
+
+-(double)intToNonestationary:(double)alf{
+    double x_temp = self.x;
+    self.x = alf;
+    double res  = (1. - [self.FLow getFWithX:(self.t - self.x - alf)])*[MathMehods fluctionFromFunction:@selector(H1:) selectorTarget:self isStatic:NO withParametr:self.x];
+    self.x = x_temp;
+    return res;
+}
+
+-(double)intToStationary:(double)x{
+    return 1 - [self.FLow getFWithX:(self.x + x)];
+}
+
+-(double)H1:(double)x{
+    int numSteps = 10000;
+    return [MathMehods solveEquationVolteraWithKernel:@selector(kernelWithX:andT:) f:@selector(complexFunctionFxG:) selectorTarget:self isStatic:NO withEndPoint:x lambda:1 andStep:x/numSteps][numSteps];
+}
+
+-(double)complexFunctionFxG:(double)x{
+    tempParametr = x;
+    return [MathMehods simpsonFromFunction:@selector(intComplexFunction:) selectorTarget:self isStatic:NO withBorder:CGPointMake(0, tempParametr) andHalfNumSteps:100000];
+}
+
+-(double)intComplexFunction:(double)x{
+    return [self.FLow getFWithX:(tempParametr - x)]*[self.GLow getfWithX:x];
+}
+
+-(double)kernelWithX:(double)x andT:(double)t{
+    
+    double temptempParametr = tempParametr;
+    tempParametr = x - t;
+    double result = [MathMehods fluctionFromFunction:@selector(complexFunctionFxG:) selectorTarget:self isStatic:NO withParametr:(x - t)];
+    tempParametr = temptempParametr;
+    
+    return result;
 }
 
 @end
